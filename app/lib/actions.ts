8b3d813c932bb167294ddb469002b50fb8e16346
Salma-3 <salmehdhar@icloud.com>
2024-03-06 15:32:15 +0300

    auth and metadata added

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -67,29 +69,35 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-  try {
-    const { customerId, amount, status } = UpdateInvoice.parse({
-      customerId: formData.get('customerId'),
-      amount: formData.get('amount'),
-      status: formData.get('status')
-    });
-  
-    const amountInCents = amount * 100;
-  
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  const validateForm = UpdateInvoice.safeParse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status')
+  });
+
+  if (!validateForm.success) {
+    return {
+      errors: validateForm.error.flatten().fieldErrors,
+      message: 'Invalid Fields',
+    };
+  }
+  const { customerId, amount, status } = validateForm.data;
+  const amountInCents = amount * 100;
+
+  try {  
     await sql `
       UPDATE invoices
       SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
       WHERE id = ${id}
     `;
-  
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
   } catch (error) {
     return {
       message: 'Database Error: Failed to update invoice',
     }
   }
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
 
 // delete invoice 
@@ -103,5 +111,26 @@ export async function deleteInvoice(id: string) {
     return {
       message: 'Database Error: Failed to delete invoice'
     }
+  }
+}
+
+// authenticate
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid Credentials';
+        default:
+          return 'Something went wrong';
+      }
+    }
+    throw error;
   }
 }
